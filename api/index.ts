46e2e625b25f723e9960fd389158ce95068952b0
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-08-23 14:39:55 -0300

    primer commit

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Express on Vercel'));
+app.get('/', (req: Request, res: Response) => res.send('Home Page Route'));
+app.get('/about', (req: Request, res: Response) =>
+    res.send('About Page Route')
+);
+app.get('/portfolio', (req: Request, res: Response) =>
+    res.send('Portfolio Page Route')
+);
+app.get('/contact', (req: Request, res: Response) =>
+    res.send('Contact Page Route')
+);
 
-// No necesitas `app.listen(...)` ya que Vercel lo maneja automáticamente.
+const port = process.env.PORT || 3000;
 
-export default app;
+app.listen(port, () =>
+    console.log(`Server running on ${port}, http://localhost:${port}`)
+);
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
